e2069b958b95d08f4f029061fa5fdc65d140d30e
Ankit Sadana <sadana.ankit@gmail.com>
2019-04-06 06:30:30 -0400

    Added function to extract pem from pfx.
    Added prefix "function" to all, just for consistency.

--- personal-env.functions.bash
+++ personal-env.functions.bash
@@ -2,7 +2,7 @@
 
 # stashes the uncommited code, pulls from origin then unstashes
 # Warning if there are conflicts it will not show in interactive UI
-branchUpdate() {
+function branchUpdate() {
 	stashRunUnstash() {
 		unCommittedChanges="Changes not staged for commit:"
 
@@ -22,12 +22,12 @@ branchUpdate() {
 	stashRunUnstash "git pull --rebase"
 }
 
-gitDiffWithBranch() {
+function gitDiffWithBranch() {
 	currentBranch=$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD))
 	git diff HEAD "${1:-$currentBranch}"
 }
 
-logDiffBetweenBranches() {
+function logDiffBetweenBranches() {
 	echo "Showing $1..$2"
 	git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset  %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative $1..$2
 	echo
@@ -35,22 +35,27 @@ logDiffBetweenBranches() {
 	git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset  %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative $2..$1
 }
 
-handBrakeConvert() {
+function handBrakeConvert() {
 	HandBrakeCLI -i "$1" -o "${2:-$(echo "$1" | sed -e 's/\(^.*[sS][0-9][0-9][eE][0-9][0-9]\)\(.*\)/\1/').mp4}" -e x264 -q 21 -B 160 && \
 	rm "$1"
 }
 
-convertAndRmAllInFolder() {
+function convertAndRmAllInFolder() {
 	for file in $(find "${2:-./}" -type f -name "*.${1:-mkv}"); do
 		handBrakeConvert "$file"
 	done
 }
 
-pullSSLCert() {
+function pullSSLCert() {
 	openssl s_client -showcerts -connect "${1}":443 < /dev/null 2> /dev/null | openssl x509 -noout -text
 }
 
 # Get keys from mit liscence keys
-getGPGKey() {
+function getGPGKey() {
 	gpg --recv-keys --keyserver hkp://pgp.mit.edu $1
+}
+
+function getCertKeyPemFromPfx() {
+	openssl pkcs12 -in "$1.pfx" -out "$1-cert.pem" -clcerts -nokeys
+    openssl pkcs12 -in "$1.pfx" -out "$1-key.pem" -nocerts -nodes
 }
