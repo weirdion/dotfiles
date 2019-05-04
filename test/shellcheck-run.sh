44915391d5ccb44c1e3f2f0ff88f7ccda811e474
Ankit Sadana <sadana.ankit@gmail.com>
2019-05-03 20:09:11 -0400

    [#1] Resolved personal-env.aliases.bash

--- test/shellcheck-run.sh
+++ test/shellcheck-run.sh
@@ -2,7 +2,8 @@
 set -o errexit
 
 # Exclude SC1090 - to avoid warnings about assumed files present that shellcheck can't read
-# Exclude SC1091 - to avoid warnings about assumed files present that shellcheck can't read
-shellcheck --shell=bash --exclude=SC1090,SC1091 personal-env*
+# Exclude SC1091 - to avoid warnings about assumed files present that shellche,ck can't read
+# Exclude SC1012 - for \r being used in sed command, the suggested behavior is incorrect for the use-case.
+shellcheck --shell=bash --exclude=SC1090,SC1091,SC1012 personal-env*
 
 exit 0
