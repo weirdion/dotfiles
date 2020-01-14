808e9fae20043a2078104b4ef00bdfc38893cb95
Ankit Sadana <sadana.ankit@gmail.com>
2020-01-13 21:25:49 -0500

    Separated function helpers to a different script file

--- personal-env.functions.bash
+++ personal-env.functions.bash
@@ -1,51 +1,6 @@
 #!/usr/bin/env bash
 
-COLOR_RED='\033[0;31m'
-COLOR_GREEN='\033[0;32m'
-COLOR_YELLOW='\033[1;33m'
-COLOR_LBLUE='\033[1;34m'
-COLOR_WHITE='\033[0;37m'
-COLOR_NONE='\033[0m'
-
-function __checkIfRoot() {
-	if [ "$(id -u)" -ne 0 ]
-	then
-	  echo -e "\nERROR: You must run this command with sudo.";
-	  exit 1;
-	fi
-}
-
-function __checkIfNotRoot() {
-	if [ "$(id -u)" -eq 0 ]
-	then
-	  echo -e "\nERROR: You must NOT use this command as sudo.";
-	  exit 1;
-	fi
-}
-
-function __printInColor() {
-	echo -e "$2$1${COLOR_NONE}"
-}
-
-function __printInRed() {
-	__printInColor "$1" "$COLOR_RED"
-}
-
-function __printInGreen() {
-	__printInColor "$1" "$COLOR_GREEN"
-}
-
-function __printInYellow() {
-	__printInColor "$1" "$COLOR_YELLOW"
-}
-
-function __printInBlue() {
-	__printInColor "$1" "$COLOR_LBLUE"
-}
-
-function __printInWhite() {
-	__printInColor "$1" "$COLOR_WHITE"
-}
+source "$(dirname "${BASH_SOURCE[0]}")/personal-env.functions-helpers.bash"
 
 function __parse_git_branch() {
 	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
