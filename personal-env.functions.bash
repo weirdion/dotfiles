9bc58db3a9a63c3f9257339859b50aaf75ecf09e
Ankit Sadana <sadana.ankit@gmail.com>
2020-01-13 21:30:33 -0500

    Separated git functions into a different file

--- personal-env.functions.bash
+++ personal-env.functions.bash
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 source "$(dirname "${BASH_SOURCE[0]}")/personal-env.functions-helpers.bash"
-
-function __parse_git_branch() {
-	git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
-}
+source "$(dirname "${BASH_SOURCE[0]}")/personal-env.functions-git.bash"
 
 function pidinfo() {
   if [ "$#" -eq 1 ]; then
@@ -13,43 +10,6 @@ function pidinfo() {
     __printInRed "Unable to execute the command."
     __printInWhite "Usage: pidname PID"
   fi
-}
-
-# stashes the uncommited code, pulls from origin then unstashes
-# Warning if there are conflicts it will not show in interactive UI
-function branchUpdate() {
-	stashRunUnstash() {
-		unCommittedChanges="Changes not staged for commit:"
-
-		if git status | grep "$unCommittedChanges" ; then
-			__printInYellow "\nSaving changes to stash\n" && \
-			git stash save && \
-			__printInYellow "\nPulling from tracking repo..\n" && \
-			$1 && \
-			__printInYellow "\nUnstashing changes from stash\n" && \
-			git stash pop
-		else
-			__printInGreen "\nNo local changes found.." && \
-			__printInGreen "\nPulling from tracking repo..\n" && \
-			$1
-		fi
-	}
-	stashRunUnstash "git pull --rebase"
-}
-
-# TODO: This should be moved to git aliases script
-function gitDiffWithBranch() {
-	currentBranch=$(git for-each-ref --format='%(upstream:short)' "$(git symbolic-ref -q HEAD)")
-	git diff HEAD "${1:-$currentBranch}"
-}
-
-# TODO: This should be moved to git aliases script
-function logDiffBetweenBranches() {
-	echo "Showing $1..$2"
-	git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset  %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative "$1".."$2"
-	echo
-	echo "Showing $2..$1"
-	git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset  %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative "$2".."$1"
 }
 
 function convertToMp4() {
