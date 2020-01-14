7c913638ad9194709fbcc51cc4abd74aa38b3ecc
Ankit Sadana <sadana.ankit@gmail.com>
2020-01-13 22:17:44 -0500

    Separated cert and remote server functions into a different file

---
+++ personal-env.functions-server.bash
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+source "$(dirname "${BASH_SOURCE[0]}")/personal-env.functions-helpers.bash"
+
+function pullSSLCert() {
+	if [ "$#" -eq 1 ]
+	then
+		openssl s_client -showcerts -connect "${1}":443 < /dev/null 2> /dev/null | openssl x509 -noout -text
+	else
+		__printInRed "Unable to execute the command."
+		__printInWhite "Usage: pullSSLCert webaddress"	
+	fi
+}
+
+function getCertKeyPemFromPfx() {
+	if [ "$#" -eq 1 ]
+	then
+		openssl pkcs12 -in "$1.pfx" -out "$1-cert.pem" -clcerts -nokeys
+    	openssl pkcs12 -in "$1.pfx" -out "$1-key.pem" -nocerts -nodes
+	else
+		__printInRed "Unable to execute the command."
+		__printInWhite "Usage: getCertKeyPemFromPfx name_of_the_pfx"
+	fi
+}
+
+function sshfs-mount() {
+	if [ "$#" -ge 2 ]; then
+		sshfs -o uid="$(id -u "$LOGNAME")" -o gid="$(id -u "$LOGNAME")" -o reconnect -o  ServerAliveInterval=15 "$1" "$2"
+	else
+		__printInRed "Unable to execute the command."
+		__printInWhite "Usage sshfs-mount [sshfs options] source destination"
+	fi
+}
+
+function sshfs-umount() {
+	if [ "$#" -eq 1 ]; then
+		fusermount3 -u "$1"
+	else
+		__printInRed "Unable to execute the command."
+		__printInWhite "Usage sshfs-umount [fusermount options] destination"
+	fi
+}
+
+function sync-folder-local2remote() {
+	if [[ "$#" -eq 2 ]]; then
+		rsync --progress -vr --ignore-existing "$1" "$2$1"
+	else
+		__printInRed "Unable to execute the command."
+		__printInWhite "Usage sync-folder-local2remote local-folder remote:path"
+		__printInWhite "Example: \`sync-folder-local2remote folder1 dummyremote:/home/user/\` will sync local folder1 with dummyremote:/home/user/folder1"
+	fi
+}
